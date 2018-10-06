8b60141bb94de5bdee9dc3c971e30db4463067f4
Will Harris <Harriswill22@yahoo.com>
2018-10-06 07:36:36 -0400

    adds coins for user input

---
+++ scripts/numberofcoins.js
@@ -0,0 +1,36 @@
+//need variables 
+//- the users' answer
+let whatUserSaid  = ``;
+//- if so, increment the count
+//how many coins they have
+let numberOfCoins = 0;
+
+
+//we need a loop
+// it needs to rn at least once
+while (whatUserSaid === `yes`) {
+
+}
+//inside the loop:
+//-ask them if they want another coin
+whatUserSaid = prompt(`Hey, want another?`);
+
+if (whatUserSaid === `yes`) {
+
+    numberOfCoins++;
+}
+debugger;
+
+console.log(`You have ${numberOfCoins} coins`);
+
+
+
+
+
+
+//-print the number od coinse
+//-print the number of coinse
+
+//after the loop (meaning, they said that they dididn't want any more coins )
+// just say bye
+console.log(`Bye Felicia`);
