fe57091e5446b78b7e98097e6a03c9e548ed407d
Will Harris <Harriswill22@yahoo.com>
2018-10-08 14:00:27 -0400

    capitalizes first letter

---
+++ scripts/capitalize.js
@@ -0,0 +1,12 @@
+// Arrays 
+// Functions 
+// "objects"
+const who_am_i = `i'm batman`;
+
+const first = who_am_i.slice(0,1).toUpperCase();
+
+const rest = who_am_i.slice(1);
+
+console.log(first + rest);
+
+console.log(`The length of the string is ${who_am_i.length}`);
